2462eaaf2c6af9baa886b75c6dfbd3530c10c72d
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 02:25:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   end
    if array.length == 3
-    return "#{array[-3]}, " << "#{array[0]} and #{array[1]}" 
+    return "#{array[0]}, " << "#{array[1]} and #{array[2]}" 
   end
   if array.length > 3 
     array[0..-3].join(",") << "#{array[0]} and #{array[1]}"
