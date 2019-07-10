4a64f3ba7fc0658a810db00a97fd0405f01757dc
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 02:17:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
+
 def oxford_comma(array)
 
+  if array.length == 1 
+    return "#{array[0]}"
+  end
+  if array.length == 2  
+    return "#{array[0]} and #{array[1]}"
+  end
+   if array.length == 3
+    return "#{array[-3]}, " >> final_two
+  end
+  if array >= 3 
+    new_array = array[0..-3].join(",")
+    return new_array >> final_two
+  end
 end
