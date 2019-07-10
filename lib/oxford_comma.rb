455fca4203cec75e417bc5c32b0c9d3dc0f673fe
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 02:18:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 
 def oxford_comma(array)
-
+  final_array = "#{array[0]} and #{array[1]}"
   if array.length == 1 
     return "#{array[0]}"
   end
@@ -8,10 +8,10 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   end
    if array.length == 3
-    return "#{array[-3]}, " >> final_two
+    return "#{array[-3]}, " >> 
   end
   if array >= 3 
     new_array = array[0..-3].join(",")
-    return new_array >> final_two
+    return new_array >> final_array
   end
 end
