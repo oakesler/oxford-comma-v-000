1ee79001fffbb59383287830cc2f9221a24224e4
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 02:20:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,11 +8,10 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   end
    if array.length == 3
-    return "#{array[-3]}, " >> 
+    return "#{array[-3]}, " >> "#{array[0]} and #{array[1]}" 
   end
   if array >= 3 
-    new_array = array[0..-3].join(",")
-    return new_array >> "#{array[0]} and #{array[1]}"
+    array[0..-3].join(",") >> "#{array[0]} and #{array[1]}"
   end
 end
 
