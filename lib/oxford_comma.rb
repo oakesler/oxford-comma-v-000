4b96c1cc233b2964015bd776b9a25258f3ecf8a4
Olivia Kesler <oakesler@protonmail.ch>
2019-07-10 02:32:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  
+  final_array = "#{array[0]} and #{array[1]}"
+  if array.length == 1 
+    return "#{array[0]}"
+  end
+  if array.length == 2  
+    return "#{array[0]} and #{array[1]}"
+  end
+   if array.length == 3
+    return "#{array[0]}, " << "#{array[1]}, and #{array[2]}" 
+  end
+  if array.length > 3 
+    array[0..-3].join(", ") << ", #{array[-2]}, and #{array[-1]}"
+  end
 end
+
